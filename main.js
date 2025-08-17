bc3ba2a91837b483086eeb362724abaaebfdd71f
rafiqmia65 <rafiqdeveloper4465@gmail.com>
2025-08-18 02:07:08 +0600

    Problem 7 solved

--- main.js
+++ main.js
@@ -159,3 +159,29 @@ const numbersSum = (arr) => {
   return totalSum;
 };
 // console.log(numbersSum([1, 2, 3, 4]))
+
+// ################################################################
+// Problem 7: Find Even Numbers in an Array
+// Write a function that returns all even numbers from a given array.
+
+// Example:
+
+// Input: [1, 2, 3, 4, 5, 6]
+// Output: [2, 4, 6]
+
+const evenNumbers = (arr) => {
+  if (!Array.isArray(arr) || arr.length === 0) {
+    return "Please Input a valid array";
+  }
+
+  let evenArray = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+      evenArray.push(arr[i]);
+    }
+  }
+
+  return evenArray;
+};
+console.log(evenNumbers([1, 2, 3, 4, 5, 6]));
